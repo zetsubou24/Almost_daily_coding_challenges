796775f3cc16ef8687dab0ca975961efcff20036
Akhil Anil M <47663052+zetsubou24@users.noreply.github.com>
2019-08-03 12:36:25 +0300

    Solved

--- Day1/Solution1.cpp
+++ Day1/Solution1.cpp
@@ -12,8 +12,7 @@ int main(){
         cin>>x;
         a.push_back(x);
     }
-    int i=0,j=n-1;
-    int ans=0;
+    int i=0,j=n-1,ans=0;
     while(i<j){
         ans=max(ans,min(a[i],a[j])*(j-i));
         if(a[i]>a[j]) j--;
